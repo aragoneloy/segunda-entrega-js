0d809cb4612a805d2e8edac62d9285d93fe707a3
Eloy Aragon <eloy.aragon@hotmail.com>
2022-03-15 21:33:02 -0300

    terminado

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import Producto from "./producto";
+import Producto from "./producto.js";
 
 let productos = []
 
@@ -8,37 +8,51 @@ if(localStorage.getItem('Productos')){
     localStorage.setItem('Productos', JSON.stringify(productos))
 }
 
+
 let formProducto = document.getElementById('convertidorPrecios')
 let btnLimpiar = document.getElementById('btnLimpiar')
 let listadoProductos = document.getElementById('listadoProductos')
 
-// let listadoProductos = listadoProductos.innerHTML
+
+
 
 formProducto = addEventListener('submit', (e) => {
     e.preventDefault()
     let producto = document.getElementById('IDproducto').value
     let precioEnDolares = document.getElementById('IDprecioEnDolares').value
     let precioEnPesos = parseFloat(precioEnDolares) * 100 * 1.35 * 1.30
-
+    
+    
     const product = new Producto(producto, precioEnDolares, precioEnPesos)
     productos.push(product)
     localStorage.setItem('Productos', JSON.stringify(productos))
     document.getElementById('convertidorPrecios').reset()
     
-    console.log(productos)
+    
 
     //mostrar listado de productos cargados en pantalla 
+    listadoProductos.innerHTML = ""
+   
     productos.forEach((productosEnArray, indice, array) => {
-        console.log(array)
+        console.log(array)    
 
-        listadoProductos.insertAdjacentHTML('beforeend', ` <li id="producto${indice}" class="list-group-item">Producto nº: ${indice + 1} - Nombre: ${productosEnArray.producto} - Precio en pesos: $${productosEnArray.precioEnPesos}</li>`)
-    })
+                
+
+        listadoProductos.insertAdjacentHTML("beforeend", ` <li id="producto${indice}" class="list-group-item">Producto nº: ${indice + 1} - 
+        Nombre: ${productosEnArray.producto} - 
+        Precio en pesos: $${productosEnArray.precioEnPesos}</li>`) 
+            })
    
 
 
 })
 
+
+    
+
+
 btnLimpiar.addEventListener('click', () => {
+    
     window.localStorage.clear()
     sessionStorage.clear()
     document.location.reload();
@@ -49,38 +63,4 @@ btnLimpiar.addEventListener('click', () => {
 
 
 
-
-// let continuar
-// do{
-//     productos.push(new Producto(prompt("Ingrese nombre del producto"), parseFloat(prompt("Ingrese precio en uS$"))))
-//     let eliminarUltimo = prompt("Confirmar carga? y/n").toLowerCase()
-//     if (eliminarUltimo == "n"){
-//         productos.pop()
-//     } else if(eliminarUltimo == "y"){
-//         continue
-//     } else {
-//         alert('Ingrese solo letras "y" o "n"')
-//         break
-//     }
-
-//     let confirma = prompt("Agregar otro producto? y/n").toLowerCase()
-//     if(confirma == "n" ){
-//         continuar = false
-//     }
- 
-// } while (continuar == true)
-
-
-// console.log(productos)
-
-//pasar los precios a pesos
-
-// const productoEnPesos = productos.map((el) => {
-//     return {
-//         nombre: el.nombre,
-//         precioEnPesos: el.precioEnDolares * 111 * 1.35 * 1.30
-//     }
-// })
-
-// console.log(productoEnPesos)
 
